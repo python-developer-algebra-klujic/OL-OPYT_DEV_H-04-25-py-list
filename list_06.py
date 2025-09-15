978ca4b4757d72b33009240c9168586e7b883ec6
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-15 19:13:24 +0200

    Update list_06.py

--- list_06.py
+++ list_06.py
@@ -12,3 +12,18 @@ numbers.append(31)
 
 number_of_elements = len(numbers)
 print(f'Lista numbers ima {number_of_elements} clanova.')
+print()
+
+
+names = []
+# names = [
+#     'Pero Peric',
+#     'Ana Anic',
+#     'Iva Ivic'
+# ]
+
+if len(names) == 0:
+    print('U kolekciji nema podataka')
+else:
+    for name in names:
+        print(name)
