0068321ff6ba9981a8211a21134484539ebd9e4b
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-15 19:10:06 +0200

    Create list_06.py

---
+++ list_06.py
@@ -0,0 +1,14 @@
+# broj elemenata liste
+
+# nacin da range konvertiramo u listu
+numbers = list(range(10))
+number_of_elements = len(numbers)
+print(f'Lista numbers ima {number_of_elements} clanova.')
+
+
+numbers.append(11)
+numbers.append(21)
+numbers.append(31)
+
+number_of_elements = len(numbers)
+print(f'Lista numbers ima {number_of_elements} clanova.')
