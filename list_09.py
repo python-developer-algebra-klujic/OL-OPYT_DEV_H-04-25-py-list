04be4615c7dd8dd1e20aaa0ee5ae8c907595a959
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-15 19:32:55 +0200

    Create list_09.py

---
+++ list_09.py
@@ -0,0 +1,16 @@
+# brojanje koliko se neki element pojavljuje u listi
+bands = [
+    'The Beatles',
+    'Pink Floyd',
+    'Pink Floyd',
+    'Queen',
+    'Pink Floyd',
+    'Nirvana',
+    'Radiohead',
+    'Metallica',
+    'Coldplay',
+    'Pink Floyd'
+]
+
+value_occurrence = bands.count('Pink Floyd')
+print(f'Pink Floyd se u listi pojavljuje {value_occurrence} puta.')
