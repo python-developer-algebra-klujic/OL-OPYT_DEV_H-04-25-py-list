be7f047dd5143420e811afd06121a6a87fa260e4
Krunoslav Lujic <krunoslav.lujic@predavaci.algebra.hr>
2025-09-15 19:03:08 +0200

    Create list_05.py

---
+++ list_05.py
@@ -0,0 +1,12 @@
+# insert in list
+
+# nacin da range konvertiramo u listu
+numbers = list(range(10))
+print(numbers)
+
+# dodaje element u listu, ali uvijek na kraju liste
+numbers.append(11)
+print(numbers)
+
+numbers.insert(4, 'jedanaest')
+print(numbers)
